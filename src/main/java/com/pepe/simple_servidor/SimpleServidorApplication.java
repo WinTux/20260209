e87eb127a7170155ed1fc83d89917130c92e58da
rusok <rusok.software@gmail.com>
2026-02-06 21:03:22 -0400

    Commit inicial

---
+++ src/main/java/com/pepe/simple_servidor/SimpleServidorApplication.java
@@ -0,0 +1,13 @@
+package com.pepe.simple_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServidorApplication.class, args);
+	}
+
+}
